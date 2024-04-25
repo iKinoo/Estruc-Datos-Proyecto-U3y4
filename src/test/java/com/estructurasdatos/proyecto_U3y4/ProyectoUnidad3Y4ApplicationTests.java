63623309a40cc24ccb1e9b3d3fb46a0c10624528
Rodrigo Pacab <rodri27red@gmail.com>
2024-04-25 17:34:02 -0600

    initial commit

---
+++ src/test/java/com/estructurasdatos/proyecto_U3y4/ProyectoUnidad3Y4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estructurasdatos.proyecto_U3y4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUnidad3Y4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
