ea6790c06884a8bede2f850ccbbcd7062c06e78e
Isaías Rodríguez <a22216371@alumnos.uady.mx>
2024-04-27 13:47:29 -0600

    Subiendo arboles ABB

---
+++ src/main/java/com/estructurasdatos/proyecto_U3y4/ItemNotFoundException.java
@@ -0,0 +1,8 @@
+package com.estructurasdatos.proyecto_U3y4;
+
+public class ItemNotFoundException extends RuntimeException {
+
+    public ItemNotFoundException(String msg) {
+        super(msg);
+    }
+}
